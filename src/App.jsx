dec524233dd5c885c7aaa8a88f60a16d2aa02d57
LoidPadre <145937391+loidpadre@users.noreply.github.com>
2024-07-02 17:25:07 +0000

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import './App.css';
-
+import 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h1>Note app
+
+      </h1>
     </div>
   );
 }
---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,5 @@
+export const GlobalStyle = {
+    primaryColor: "#8981D8",
+    secondColor: "#FFFFFF",
+    background: "#F9F9F9"
+}
