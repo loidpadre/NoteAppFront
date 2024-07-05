f079e76bee259c9f26658208dfd82b7d4f442a59
LoidPadre <145937391+loidpadre@users.noreply.github.com>
2024-07-05 19:56:26 +0000

    lateral menu design

--- src/components/MenuLateral/index.jsx
+++ src/components/MenuLateral/index.jsx
@@ -1,12 +1,45 @@
 import { Wrapper } from "./style"
-
+import IMG from "../../assets/image (2).png"
+import { BiSupport } from "react-icons/bi";
+import { IoDocumentAttachSharp } from "react-icons/io5";
+import { GrNotes } from "react-icons/gr";
+import { FaTasks } from "react-icons/fa";
+import { GrOverview } from "react-icons/gr";
 export const MenuLateral = () =>{
     return(
         <Wrapper>
             <div className="content">
-            <h1>Logo</h1>
+            <div className="logo">
+                <h1>NOTES</h1>
+            </div>
             
-                <h1>Settings</h1>
+                <div className="setting">
+                <ul>
+                    <li>
+                        <GrOverview/>
+                        Overview
+                        </li>
+                    <li>
+                        <FaTasks/>
+                        Task
+                        </li>
+                    <li>
+                    <IoDocumentAttachSharp />
+                        Documentação
+                        </li>
+                    <li>
+                    <GrNotes />
+                        Notas
+                        </li>
+                    <li>
+                    <BiSupport />
+                        Support
+                        </li>
+                </ul>
+                </div>
+                <div className="redes">
+
+                </div>
             </div>
         </Wrapper>
     )
--- src/components/MenuLateral/style.jsx
+++ src/components/MenuLateral/style.jsx
@@ -5,8 +5,44 @@ export const Wrapper = styled.nav`
     height: 100vh;
     background-color: ${GlobalStyle.primaryColor} ;
     color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
     .content{
-        
+        margin-top: 40px;
+        .logo{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            img{
+                width: 100px;
+                border-radius: 50%;
+                height: 100px;
+                object-fit: cover;
+            }
+        }
+        .setting{
+            ul{
+                li{
+                    display: flex;
+                    gap: 20px;
+                    list-style: none;
+                    margin-top: 35px;
+                    font-size: 16px;
+                    font-weight: 400;
+                    transition: 0.5s ease; 
+                    align-items: center;
+                    cursor: pointer;
+                    padding: 8px;
+                    border-radius: 8px;
+                    &:hover{
+                        background-color: white;
+                        color: ${GlobalStyle.primaryColor};
+                    }
+                }
+            }
+        }
         /// vai entrar os estylos das definiçoes
     }
 `
